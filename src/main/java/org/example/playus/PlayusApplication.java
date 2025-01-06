4f2972ae0a45c1d775c8759a842272cb334fa220
W.llama <tmdqja96@gmail.com>
2025-01-06 15:21:41 +0900

    init

---
+++ src/main/java/org/example/playus/PlayusApplication.java
@@ -0,0 +1,13 @@
+package org.example.playus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlayusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlayusApplication.class, args);
+    }
+
+}
