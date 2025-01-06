7a09fa429d91eb3f1e40b96e6dd51bda5b8c17cd
WonGi-Kim <kwg970310@gmail.com>
2025-01-06 16:10:54 +0900

    fix : spreadSheetId 값을 숨긴 상태로 변경

--- src/main/java/domain/sheet/GoogleSheetController.java
+++ src/main/java/domain/sheet/GoogleSheetController.java
@@ -1,6 +1,7 @@
 package domain.sheet;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,14 +14,16 @@ public class GoogleSheetController {
 
     private final GoogleSheetService googleSheetService;
 
-    // TODO : ID ignore에 추가 필요
-    private static final String SPREADSHEET_ID = "1TcVHmKbN0mWorIEdYdqVeunCgxEw2AOHjANaa6rvIX4"; // 스프레드시트 ID
+    @Value("${google.spreadsheet.id}")
+    private String spreadSheetId; // 스프레드시트 ID
+
+    // TODO : 셀 범위를 수정해야 함
     private static final String RANGE = "요구사항!B2:D2"; // 읽을 셀 범위
 
     @GetMapping("/read")
     public ResponseEntity<List<Object>> readFromSheet() {
         try {
-            List<Object> data = googleSheetService.getSheetData(SPREADSHEET_ID, RANGE);
+            List<Object> data = googleSheetService.getSheetData(spreadSheetId, RANGE);
             return ResponseEntity.ok(data);
         } catch (Exception e) {
             e.printStackTrace();
