421fce7ccd4096363113462a84d9d7b06649ca0e
WonGi-Kim <kwg970310@gmail.com>
2025-01-07 20:11:07 +0900

    add : CommonResponse 생성 및 핸들러 추가

---
+++ src/main/java/org/example/playus/global/exception/ErrorCode.java
@@ -0,0 +1,32 @@
+package org.example.playus.global.exception;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@RequiredArgsConstructor
+public enum ErrorCode {
+
+    BAD_REQUEST(HttpStatus.BAD_REQUEST, "BAD REQUEST"),
+    FORBIDDEN(HttpStatus.FORBIDDEN, "FORBIDDEN"),
+    UNAUTHORIZED(HttpStatus.UNAUTHORIZED, "UNAUTHORIZED"),
+    NOT_FOUND(HttpStatus.NOT_FOUND, "NOT FOUND"),
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "INTERNAL SERVER ERROR"),
+
+    // Token
+    INVALID_TOKEN(HttpStatus.UNAUTHORIZED, "유효하지 않은 JWT 토큰입니다."),
+    TOKEN_EXPIRATION(HttpStatus.UNAUTHORIZED, "만료된 토큰입니다. 재로그인 해주세요."),
+    NOT_SUPPORTED_TOKEN(HttpStatus.UNAUTHORIZED, "지원되지 않는 JWT 토큰입니다."),
+    FALSE_TOKEN(HttpStatus.BAD_REQUEST, "잘못된 JWT 토큰입니다."),
+    HEADER_NOT_FOUND(HttpStatus.BAD_REQUEST, "헤더가 잘못되었거나 누락되었습니다."),
+    UNMATCHED_TOKEN(HttpStatus.BAD_REQUEST, "일치하지 않는 토큰입니다."),
+
+    // USER
+    USER_NOT_FOUND(HttpStatus.BAD_REQUEST, "유저를 찾을 수 없습니다."),
+    USER_ALREADY_EXIST(HttpStatus.CONFLICT, "이미 같은 이름을 가진 유저가 존재합니다."),
+    PASSWORD_NOT_CORRECT(HttpStatus.BAD_REQUEST, "패스워드가 일치하지 않습니다.");
+
+    private final HttpStatus status;
+    private final String message;
+}
