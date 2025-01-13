ccd0c0d3db91d83e2e34dd5cf2cfb06e3248f06a
WonGi-Kim <kwg970310@gmail.com>
2025-01-13 19:37:56 +0900

    fix : 로그인 한 유저 정보 Context에서 바로 가져오도록

--- src/main/java/org/example/playus/domain/employee/EmployeeController.java
+++ src/main/java/org/example/playus/domain/employee/EmployeeController.java
@@ -1,8 +1,11 @@
 package org.example.playus.domain.employee;
 
 import lombok.RequiredArgsConstructor;
+import org.example.playus.domain.security.service.UserDetailsImpl;
 import org.example.playus.global.common.CommonResponse;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,10 +18,10 @@ public class EmployeeController {
     private final EmployeeService employeeService;
 
     @GetMapping("/info/exp")
-    public ResponseEntity<CommonResponse> getEmployeeExp() {
+    public ResponseEntity<CommonResponse> getEmployeeExp(@AuthenticationPrincipal UserDetailsImpl userDetails) {
         try {
-            int employeeId = 2018020101;
-            EmployeeExpReponseDto responseDto = employeeService.getEmployeeExp(employeeId);
+            Employee employee = userDetails.getEmployee();
+            EmployeeExpReponseDto responseDto = employeeService.getEmployeeExp(Integer.parseInt(employee.getEmployeeId()));
             CommonResponse response = new CommonResponse<>("회원 경험치 조회", 200, responseDto);
             return ResponseEntity.ok(response);
         } catch (Exception e) {
