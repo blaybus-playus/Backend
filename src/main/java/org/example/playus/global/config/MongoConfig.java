c563d13cc3b3b80a080f265646377cf1edb502d3
W.llama <tmdqja96@gmail.com>
2025-01-13 18:31:30 +0900

    feat mongodb package 추가

--- src/main/java/org/example/playus/global/config/MongoConfig.java
+++ src/main/java/org/example/playus/global/config/MongoConfig.java
@@ -16,7 +16,8 @@ import org.springframework.data.mongodb.repository.config.EnableMongoRepositorie
         "org.example.playus.domain.project",
         "org.example.playus.domain.evaluation",
         "org.example.playus.domain.employeeExp",
-        "org.example.playus.domain.level"
+        "org.example.playus.domain.level",
+        "org.example.playus.domain.admin"
 })
 public class MongoConfig {
 
