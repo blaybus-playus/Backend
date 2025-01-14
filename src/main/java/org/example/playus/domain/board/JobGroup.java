162e87f5c0e6903a0d26b6111dfed896ec4bd57e
W.llama <tmdqja96@gmail.com>
2025-01-14 11:21:05 +0900

    feat 직군 컬럼 추가

---
+++ src/main/java/org/example/playus/domain/board/JobGroup.java
@@ -0,0 +1,21 @@
+package org.example.playus.domain.board;
+
+import lombok.Getter;
+
+@Getter
+public enum JobGroup {
+    T("기술직군"),
+    F("현장직군"),
+    B("관리직군"),
+    G("성장전략"),
+    ;
+    private final String description;
+
+    JobGroup(String description) {
+        this.description = description;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+}
--- src/main/java/org/example/playus/domain/board/dto/BoardResponseDto.java
+++ src/main/java/org/example/playus/domain/board/dto/BoardResponseDto.java
@@ -3,6 +3,7 @@ package org.example.playus.domain.board.dto;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.example.playus.domain.board.JobGroup;
 
 @Getter
 @NoArgsConstructor
@@ -11,14 +12,16 @@ public class BoardResponseDto {
     private String id;       // 게시글 ID
     private String title;    // 게시글 제목
     private String content;  // 게시글 내용
+    private JobGroup jobGroup;
     private boolean success; // 요청 성공 여부
     private String message;  // 응답 메시지
 
     @Builder
-    public BoardResponseDto(String id, String title, String content, boolean success, String message) {
+    public BoardResponseDto(String id, String title, String content,JobGroup jobGroup ,boolean success, String message) {
         this.id = id;
         this.title = title;
         this.content = content;
+        this.jobGroup = jobGroup;
         this.success = success;
         this.message = message;
     }
